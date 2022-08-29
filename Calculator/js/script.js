5f065236852698e98e0f7579b0882a530447bfd2
Priyanshu Udainiya <109417573+priyanshuudainiya@users.noreply.github.com>
2022-08-29 15:24:13 +0530

    Add files via upload

---
+++ Calculator/js/script.js
@@ -0,0 +1,32 @@
+
+function clearScreen() {
+    document.getElementById("output").value = "";
+}
+ 
+
+function display(value) {
+    document.getElementById("output").value += value;
+}
+ 
+
+function calculate() {
+    var p = document.getElementById("output").value;
+    var q = eval(p);
+    document.getElementById("output").value = q;
+}
+
+function clearScreen() {
+    document.getElementById("output").value = "";
+}
+
+function display(value) {
+    document.getElementById("output").value += value;
+}
+
+function calculate() {
+    var p = document.getElementById("output").value;
+    var q = eval(p);
+    document.getElementById("output").value = q;
+}
+
+
