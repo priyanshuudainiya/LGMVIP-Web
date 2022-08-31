0a24e515393c2cbd5348417c1a221b60920c0a26
Priyanshu Udainiya <109417573+priyanshuudainiya@users.noreply.github.com>
2022-08-31 15:43:36 +0530

    Update aboutus.php

--- Student Result Management System/aboutus.php
+++ Student Result Management System/aboutus.php
@@ -39,10 +39,10 @@
         </div>
       </nav>
 
-      <div class="main-content-header">
-        <h3 class="search"> Student Result Management System </h3>
+      <div class="abt_us">
+        <h3 class="abt_main"> Student Result Management System </h3>
         <hr id="abt_hr">
-           <div class="div2">
+           <div>
            <p>Student Result Management System is a technological opportunity for the school, college, university and coaching centre institutions searching for a secure, simple and alternative solution to the conventional paper-based exam results evaluation, reporting and distribution. Like any other software, the system comes with certain advantage and disadvantage. <br> The software application unbelievably unravels and quickens the result management system with unique templates by providing the administration a secure database system for storing, evaluating and publishing the test scores and grades of candidates online. The database likewise allows the students to observe and gander at the exam results on the web at whatever point necessary. <br> The software exhibits some effortlessness in solving, adjusting and eradicating the problems in traditional result and examination management with automation system. One of the principal purposes of the system is to give the test results to the students in the as fundamental and exact way as possible. The administrator, teacher, professors, and staff can likewise modify and assess marks for the students whenever needed.</p>
            </div>  
         </div>
